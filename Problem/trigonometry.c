925186665e26d899204b61fa25120f43611901e7
Olukayode Ifeoluwa Joshua <ifeoluwajoshua2015@gmail.com>
2022-08-04 05:23:04 +0100

    trigonometry problem

---
+++ Problem/trigonometry.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+double a;
+double b;
+double c;
+printf("Input the value of a(opposite) = ");
+scanf("%lf", &a);
+printf("Input the value of b(adjacent) = ");
+scanf("%lf", &b);
+c = sqrt((pow(a,2)) + (pow(b,2)));
+printf("\nThe hypotenous(c) of the triangle is %lf", c);
+return 0;
+}
